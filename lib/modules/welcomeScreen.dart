e6928638bd0af313fa252d6b180ad77bfc0ce23d
webrror <jerinjacob0206@gmail.com>
2023-02-02 10:54:43 +0530

    used setstate in getDetails method

--- lib/modules/welcomeScreen.dart
+++ lib/modules/welcomeScreen.dart
@@ -5,58 +5,64 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutterapp1/services/loginService.dart';
 
 class WelcomeScreen extends StatefulWidget {
-
-   WelcomeScreen({Key? key}) : super(key: key);
+  const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   State<WelcomeScreen> createState() => _WelcomeScreenState();
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  final Authentication auth = Authentication();
+  final _storage = const FlutterSecureStorage();
+  var img;
+
+  void getDetails() async {
+    var store = await _storage.read(key: 'userDetails');
+    setState(() {
+      if (store != null) {
+        img = jsonDecode(store);
+      }
+    });
+  }
+
   @override
   void initState() {
-    // TODO: implement initState
     getDetails();
     super.initState();
   }
-  var Auth=Authentication();
-
-  final Authentication auth = Authentication();
-
-  final _storage= const FlutterSecureStorage();
-
-  var img;
-  var name;
 
   @override
   Widget build(BuildContext context) {
-
-
-
-    return Scaffold(appBar: AppBar(),
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('welcome ${img['name']}'),
-          SizedBox(height: 15,),
-          CircleAvatar(
-            minRadius: 25,
-            maxRadius: 35
-            ,
-            backgroundImage:NetworkImage(img['imgUrl'],),),
-          SizedBox(height: 10,),
-          ElevatedButton(onPressed: (){
-            auth.logOut(context);
-          }, child: Text('sign Out'))
-        ],
-      ),),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            img == null ? const Text(" ") : Text('welcome ${img['name']}'),
+            SizedBox(
+              height: 15,
+            ),
+            img == null
+                ? const SizedBox()
+                : CircleAvatar(
+                    minRadius: 25,
+                    maxRadius: 35,
+                    backgroundImage: NetworkImage(
+                      img['imgUrl'],
+                    ),
+                  ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  auth.logOut(context);
+                },
+                child: Text('sign Out'))
+          ],
+        ),
+      ),
     );
-
-  }
-  void getDetails()async{
-    var store = await _storage.read(key: 'userDetails');
-    img = jsonDecode(store!);
-
-print(img['imgUrl']);
   }
 }
